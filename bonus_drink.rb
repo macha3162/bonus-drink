5e59f07eb22a51349d8b6dcb263f5495ecd4a958
masuda <masuda.shigeki@synergy101.jp>
2016-06-29 08:24:09 +0900

    301_question1 駄菓子屋さんのオマケを求めるロジック作成

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    amount + (amount-1).abs / 2
   end
+end
+
+if __FILE__ == $0
+  amount = ARGV[0].nil? ? 100 : ARGV[0].to_i
+  answer = BonusDrink.total_count_for(amount)
+  puts "#{amount}本購入時に#{answer}本飲めます"
 end
